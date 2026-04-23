a8a1ff004787b17c267f6f282b82b8fc6ae75b54
zeigenbrod <zoeeig161@gmail.com>
2026-04-23 14:39:51 -0400

    new training llm

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,16 +1,16 @@
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, Trainer, TrainingArguments
+from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments, DataCollatorForLanguageModeling
 from datasets import load_dataset
 
 # loads the dataset
-dataset = load_dataset("text", data_files={"train": "password_dataset.txt"})
+dataset = load_dataset("text", data_files={"train": "adversarial_dataset_10k.txt"})
 
-# load tokenizer
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-
-# pad token that is used to make different length passwords, gpt-2 wants to make same length passwords
+#load tokenizer
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
 tokenizer.pad_token = tokenizer.eos_token
 
-# tokenize data
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
+
+# tokenize
 def tokenize(example):
     encodings = tokenizer(
         example["text"],
@@ -18,37 +18,32 @@ def tokenize(example):
         padding="max_length",
         max_length=16
     )
-    
     encodings["labels"] = encodings["input_ids"].copy()
     return encodings
 
 tokenized_dataset = dataset.map(tokenize, batched=True)
 
-# load model
-model = GPT2LMHeadModel.from_pretrained("gpt2")
-
 # training settings
 training_args = TrainingArguments(
-    output_dir="./gpt2-passwords",
-    num_train_epochs=2,
-    per_device_train_batch_size=8,
-    save_steps=500,
-    save_total_limit=2,
+    output_dir="./mistral-passwords",
+    num_train_epochs=1,
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=8,
     logging_steps=100,
-    learning_rate=5e-5,
+    save_steps=500,
+    learning_rate=2e-5,
     weight_decay=0.01
 )
 
-# trainer
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=tokenized_dataset["train"]
+    train_dataset=tokenized_dataset["train"],
 )
 
 # train
 trainer.train()
 
 # save model & tokenizer to folder
-trainer.save_model("./gpt2-passwords")
-tokenizer.save_pretrained("./gpt2-passwords")
+trainer.save_model("./mistral-passwords")
+tokenizer.save_pretrained("./mistral-passwords")
